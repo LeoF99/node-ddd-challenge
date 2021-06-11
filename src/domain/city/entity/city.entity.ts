613eda860e462aff3dc04d6781cf95509cf9470c
LeoF99 <leo.ferreira@dcx.ufpb.br>
2021-06-11 14:48:36 -0300

    feat: add city entity interface and class Changes to be committed: 	new file:   src/domain/city/entity/city.entity.ts 	new file:   src/domain/city/entity/interface/city.entity.interface.ts

---
+++ src/domain/city/entity/city.entity.ts
@@ -0,0 +1,27 @@
+import ICity from './interface/city.entity.interface';
+
+class City {
+  private readonly id?: number;
+
+  private readonly name: string;
+
+  private readonly state: string;
+
+  constructor(city: ICity) {
+    this.id = city.id;
+    this.name = city.name;
+    this.state = city.state;
+  }
+
+  public getId(): number | undefined {
+    return this.id;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getState(): string {
+    return this.state;
+  }
+}
---
+++ src/domain/city/entity/interface/city.entity.interface.ts
@@ -0,0 +1,9 @@
+interface ICity {
+  id?: number;
+
+  name: string;
+
+  state: string;
+}
+
+export default ICity;
