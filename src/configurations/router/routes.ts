bf19639bfb07fa75b95369372c1d0b463f5e9648
LeoF99 <leo.ferreira@dcx.ufpb.br>
2021-06-15 21:58:54 -0300

    feat: add client routes to router Changes to be committed: 	modified:   src/configurations/router/routes.ts

--- src/configurations/router/routes.ts
+++ src/configurations/router/routes.ts
@@ -1,16 +1,21 @@
 import { Router } from 'express';
 
 import CityController from '../../application/controllers/city.controller';
+import ClientController from '../../application/controllers/client.controller';
 import CityControllerFactory from '../factories/city.controller.factory';
+import ClientControllerFactory from '../factories/client.controller.factory';
 
 class Routes {
   private readonly router: Router;
 
   private readonly cityController: CityController;
 
+  private readonly clientController: ClientController;
+
   constructor() {
     this.router = Router();
     this.cityController = CityControllerFactory.getInstance();
+    this.clientController = ClientControllerFactory.getInstance();
     this.setRoutes();
   }
 
@@ -19,9 +24,15 @@ class Routes {
   }
 
   private setRoutes(): void {
-    this.router.post('/city', this.cityController.create);
-    this.router.get('/city/name', this.cityController.findByName);
-    this.router.get('/city/state', this.cityController.findByState);
+    this.router.post('/cities', this.cityController.create);
+    this.router.get('/cities/name', this.cityController.findByName);
+    this.router.get('/cities/state', this.cityController.findByState);
+
+    this.router.post('/clients', this.clientController.create);
+    this.router.get('/clients/name', this.clientController.findByName);
+    this.router.get('/clients', this.clientController.findById);
+    this.router.delete('/clients', this.clientController.remove);
+    this.router.patch('/clients/:id', this.clientController.updateName);
   }
 }
 
