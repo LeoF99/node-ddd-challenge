719e3c5c495dc5591510fbbc118d993578856c5c
LeoF99 <leo.ferreira@dcx.ufpb.br>
2021-06-15 21:53:23 -0300

    feat: add client controller factory Changes to be committed: 	new file:   src/configurations/factories/client.controller.factory.ts

---
+++ src/configurations/factories/client.controller.factory.ts
@@ -0,0 +1,20 @@
+import ClientController from '../../application/controllers/client.controller';
+import ClientService from '../../domain/client/service/client.service';
+import CityRepository from '../../infrastructure/repository/typeorm/city/city.repository';
+import ClientRepository from '../../infrastructure/repository/typeorm/client/client.repository';
+
+class ClientControllerFactory {
+  private static instance: ClientController;
+
+  public static getInstance() {
+    if (!this.instance) {
+      this.instance = new ClientController(
+        new ClientService(new ClientRepository(), new CityRepository()),
+      );
+    }
+
+    return this.instance;
+  }
+}
+
+export default ClientControllerFactory;
